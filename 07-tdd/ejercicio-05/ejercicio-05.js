bde13e0e7aa2c3c1564c3868b3c2e7746280c734
Brenda Sánchez <sanrey254@hotmail.com>
2018-06-30 22:17:54 -0500

    Completando ejercicios 07-tdd

--- 07-tdd/ejercicio-05/ejercicio-05.js
+++ 07-tdd/ejercicio-05/ejercicio-05.js
@@ -1,5 +1,14 @@
 function addOdd(n) {
-    // Escribe tu código aquí
+    let result = 0;
+	if(n === undefined || n <= 0){
+		return 'Error';
+	}
+    for (var i = 0; i <= n*2; i++) {
+    	if(i % 2 !== 0){
+    		result += i;
+    	}
+    }
+    return result;
 }
 
 if (typeof exports !== 'undefined') {
