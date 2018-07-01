bf5c118ee3912084187262f1c008021af21b8a34
Brenda Sánchez <sanrey254@hotmail.com>
2018-06-30 22:18:31 -0500

    Completando ejercicios 07-tdd

--- 07-tdd/ejercicio-06/ejercicio-06.js
+++ 07-tdd/ejercicio-06/ejercicio-06.js
@@ -1,5 +1,24 @@
 function addTwoOdd(n) {
-  // Escribe tu código aquí
+  let result = 0;
+	let result2 = 0;
+	if(n === undefined || m ===  undefined){
+		return 'Error';
+	}
+	if(n < 1 || m < 1){
+		return 'Error';
+	}
+    for (var i = 0; i <= n*2; i++) {
+    	if(i % 2 !== 0){
+    		result += i;
+    	}
+    }
+
+    for (var i = 0; i <= m*2; i++) {
+    	if(i % 2 !== 0){
+    		result2 += i;
+    	}
+    }
+    return result + result2;
 }
 
 if (typeof exports !== 'undefined') {
