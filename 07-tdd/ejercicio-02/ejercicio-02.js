8be02fba225d42ea08db64db533d239764552fc1
Brenda Sánchez <sanrey254@hotmail.com>
2018-06-30 22:14:40 -0500

    Completando ejercicios 07-tdd

--- 07-tdd/ejercicio-02/ejercicio-02.js
+++ 07-tdd/ejercicio-02/ejercicio-02.js
@@ -1,6 +1,16 @@
 function busquedaLinear (arreglo, itemBuscado) {
-    // Escribe tu código aquí
-    
+   if(arreglo === undefined || itemBuscado === undefined){
+		return 'Error';
+	}
+	if(typeof(itemBuscado) !== 'number'){
+		return 'Error de dato';
+	}
+    for (var i = 0; i <= arreglo.length - 1; i++) {
+    	if(itemBuscado === arreglo[i]){
+    		return i;
+    	}
+    }
+    return false;     
 }
 
 // NO TOCAR ESTE CÓDIGO O EXPLOTARÁ LA PC EN 10 SEGUNDOS
