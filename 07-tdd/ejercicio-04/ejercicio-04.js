4552fbb59d48522e10636376209cdd79b95460d8
Brenda Sánchez <sanrey254@hotmail.com>
2018-06-30 22:17:11 -0500

    Completando ejercicios 07-tdd

--- 07-tdd/ejercicio-04/ejercicio-04.js
+++ 07-tdd/ejercicio-04/ejercicio-04.js
@@ -1,5 +1,21 @@
 function fibonacci (limite) {
-    // Escribe tu código aquí
+    if(limite <= 0){
+		return 'Error';
+	}
+	if(typeof(limite) === 'string'){
+		return 'Error';
+	}
+
+	let num1 = 1, num2 = 0;
+	let result =[];
+	result[0] = 0;
+    for (var i= 0; i< limite-1; i++) {
+
+  		num2 = num1;
+  		num1 = num1 + result[i];
+  		result[i+1] = num2;
+    }
+    return result;
 }
 
 // NO TOCAR ESTE CÓDIGO O EXPLOTARÁ LA PC EN 10 SEGUNDOS
